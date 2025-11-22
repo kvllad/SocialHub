713eac0d64e805528abf25debee48044c2abd422
vlad <vladk6813@gmail.com>
2025-11-22 14:04:26 +0300

    fix: cors

--- api/main.py
+++ api/main.py
@@ -14,14 +14,7 @@ app = fastapi.FastAPI(title="Brainrot API", version="1.0.0")
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:8080",
-        "http://localhost:5173",
-        "http://127.0.0.1:3000",
-        "http://127.0.0.1:8080",
-        "http://127.0.0.1:5173",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
